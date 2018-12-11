a24515104ae81b2b15cc69970bbd35846f16c037
Waxiwns <mxmci525@gmail.com>
2018-12-11 23:05:30 +0200

    Snake 2/20

--- 8.Games/src/com/javarush/games/snake/SnakeGame.java
+++ 8.Games/src/com/javarush/games/snake/SnakeGame.java
@@ -3,11 +3,25 @@ package com.javarush.games.snake;
 import com.javarush.engine.cell.*;
 
 public class SnakeGame extends Game {
-    public static final int WIDTH = 15;
+    public static final int WIDTH = 20;
     public static final int HEIGHT = 15;
 
     @Override
     public void initialize() {
         setScreenSize(WIDTH, HEIGHT);
+        createGame();
+    }
+
+    private void createGame(){
+        drawScene();
+    }
+
+    private void drawScene(){
+        // разукрашиваем все ячейки
+        for (int i = 0; i < HEIGHT; i++) {
+            for (int j = 0; j < WIDTH; j++) {
+                setCellColor(j, i, Color.GREENYELLOW);
+            }
+        }
     }
 }
