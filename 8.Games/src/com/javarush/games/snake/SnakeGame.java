13a2a823cd8c3c8cfbcd6a584c777038aea52214
Waxiwns <mxmci525@gmail.com>
2018-12-11 23:57:16 +0200

    Snake 4/20

--- 8.Games/src/com/javarush/games/snake/SnakeGame.java
+++ 8.Games/src/com/javarush/games/snake/SnakeGame.java
@@ -5,6 +5,7 @@ import com.javarush.engine.cell.*;
 public class SnakeGame extends Game {
     private static final int WIDTH = 20;
     private static final int HEIGHT = 15;
+    private Color cellColor = Color.GREENYELLOW;
 
     @Override
     public void initialize() {
@@ -14,13 +15,15 @@ public class SnakeGame extends Game {
 
     private void createGame(){
         drawScene();
+        Apple apple = new Apple(7, 7);
+        apple.draw(this);
     }
 
     private void drawScene(){
         // разукрашиваем все ячейки
         for (int i = 0; i < HEIGHT; i++) {
             for (int j = 0; j < WIDTH; j++) {
-                setCellColor(j, i, Color.GREENYELLOW);
+                setCellColor(j, i, cellColor);
             }
         }
     }
