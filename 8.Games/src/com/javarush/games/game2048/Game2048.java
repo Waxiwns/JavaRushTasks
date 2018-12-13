6fe1856ceb0d94708e328ed34ba732e54058344e
Waxiwns <mxmci525@gmail.com>
2018-12-13 15:06:26 +0200

    2048 3/18

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -5,9 +5,19 @@ import com.javarush.engine.cell.*;
 public class Game2048 extends Game {
     private static final int GOAL = 2048;
     private static final int SIDE = 4;
+    private int[][] gameField = new int[SIDE][SIDE];
 
     @Override
     public void initialize() {
         setScreenSize(SIDE, SIDE);
+        createGame();
+    }
+
+    private void createGame(){
+        for (int i = 0; i < gameField.length; i++) {
+            for (int j = 0; j < gameField[1].length; j++) {
+                gameField[i][j] = 0;
+            }
+        }
     }
 }
