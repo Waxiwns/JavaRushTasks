2a099fd8bcb47ea0fb6c44ea5353ed09a081ddd6
Waxiwns <mxmci525@gmail.com>
2018-12-13 14:24:13 +0200

    2048 1/18

---
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -0,0 +1,9 @@
+package com.javarush.games.game2048;
+
+import com.javarush.engine.cell.*;
+
+public class Game2048 extends Game {
+    private static final int GOAL = 2048;
+    public static final int WIDTH = 20;
+    public static final int HEIGHT = 15;
+}
