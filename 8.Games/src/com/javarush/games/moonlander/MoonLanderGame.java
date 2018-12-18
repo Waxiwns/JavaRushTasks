b90e82d86da177a3e2c1f32e57518eecab397e0b
Waxiwns <mxmci525@gmail.com>
2018-12-18 13:32:35 +0200

    MoonLander 1/23

--- 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
+++ 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
@@ -3,11 +3,22 @@ package com.javarush.games.moonlander;
 import com.javarush.engine.cell.*;
 
 public class MoonLanderGame extends Game {
-    public static final int WIDTH = 64;
-    public static final int HEIGHT = 64;
+    public static final int WIDTH = 64;     // x - ширина
+    public static final int HEIGHT = 64;    // y - высота
 
     @Override
     public void initialize() {
-        setScreenSize(WIDTH, HEIGHT);
+        setScreenSize(WIDTH, HEIGHT); // меняем размер игрового бокса
+        createGame();                 // создание игры
+    }
+
+    // создание игры
+    private void createGame(){
+        drawScene();    // отрисовка
+    }
+
+    // отрисовка игры
+    private void drawScene(){
+
     }
 }
