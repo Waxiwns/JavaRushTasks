4675d53292a2eeff13ff3732285d3da9cef65010
Waxiwns <mxmci525@gmail.com>
2019-01-04 10:19:33 +0200

    Игра Racer (3/28)

--- 8.Games/src/com/javarush/games/racer/RacerGame.java
+++ 8.Games/src/com/javarush/games/racer/RacerGame.java
@@ -5,10 +5,28 @@ import com.javarush.engine.cell.*;
 public class RacerGame extends Game {
     public static final int WIDTH = 64;     // x - ширина
     public static final int HEIGHT = 64;    // y - высота
+    public static final int CENTER_X = WIDTH / 2;    // x - координата разделительной полосы
+    public static final int ROADSIDE_WIDTH = 4;    // x - координата обочины
 
     @Override
     public void initialize() {
         setScreenSize(WIDTH, HEIGHT);
         showGrid(false);
+        createGame();
+    }
+
+    //  старт новой игры
+    private void createGame(){
+        drawScene();
+    }
+
+    // отрисовка всех игровых объектов
+    private void drawScene(){
+        drawField();
+    }
+
+    // отрисовка фона игрового поля
+    private void drawField(){
+
     }
 }
